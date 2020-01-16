aa35dd09ebbff45070d2de0cbfef07b4ee1c6cae
Stanley Jin <stanley@shebah.com.au>
2020-01-16 22:59:50 +1100

    Initial Commit

---
+++ Algorithms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Algorithms
+//
+//  Created by Stanley Jin on 16/1/20.
+//  Copyright © 2020 Stanley Jin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
